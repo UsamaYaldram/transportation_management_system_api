0eedb8c479e2bf919654c29c84a96c9cb6602247
usamatahir <usamatahir159@gmail.com>
2025-01-11 00:15:55 +0500

    add validation not assign truck to same driver twice

--- app/models/driver_truck_assignment.rb
+++ app/models/driver_truck_assignment.rb
@@ -3,4 +3,6 @@
 class DriverTruckAssignment < ApplicationRecord
   belongs_to :driver
   belongs_to :truck
+
+  validates :driver_id, uniqueness: { scope: :truck_id, message: 'has already been assigned to this truck' }
 end
