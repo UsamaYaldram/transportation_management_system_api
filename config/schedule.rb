2d510573e7851cdce4ffe70bb80487238bae1c2d
usamatahir <usamatahir159@gmail.com>
2025-01-12 00:43:34 +0500

    fix: Issue with the Whenever gem. Could not find bundler

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,10 @@
 
 set :output, "log/cron.log" # Logs the cron job output to a file
 
+set :environment, ENV['RAILS_ENV'] || 'development'
+
+env :PATH, ENV['PATH']
+
 every 1.day, at: '12:00 am' do
   rake "sidekiq:fetch_trucks"
 end
