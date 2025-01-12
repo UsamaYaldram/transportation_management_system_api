975b46cd6ccd6f0c3abdc699a449f94b9b76e26d
usamatahir <usamatahir159@gmail.com>
2025-01-12 12:33:07 +0500

    display truck assign date for driver

--- app/serializers/truck_serializer.rb
+++ app/serializers/truck_serializer.rb
@@ -6,5 +6,14 @@ class TruckSerializer
   set_type :truck
   attributes :name, :truck_type
 
-  has_many :drivers
+  # has_many :drivers    // comment this to hide relationship in response data
+
+  attribute :driver_assignments do |truck|
+    truck.drivers.map do |driver|
+      {
+        driver_id: driver.id,
+        assign_date: truck.driver_truck_assignments.find_by(driver_id: driver.id)&.created_at&.strftime('%Y-%m-%d')
+      }
+    end
+  end
 end
